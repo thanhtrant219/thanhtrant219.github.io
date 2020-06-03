f266a454a40bd93e5f7cf4a399fbafb36b73d03e
Thanh <thanhtrant219@gmail.com>
2020-06-03 13:29:51 -0500

    Testing Website

---
+++ myScript.js
@@ -0,0 +1,8 @@
+var headerID = d3.select("#headerID");
+console.log(headerID.text());
+headerID.text("D3 had been completed");
+// d3.select();
+// d3.selectAll();
+// d3.select('hi').style('color','red')
+// .attr('calss', 'heading');
+// .text('updated hello');
