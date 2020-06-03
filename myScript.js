022ec2f4fc7fe2c4bbc1b716466f95b121e756f1
thanhtrant219 <63218294+thanhtrant219@users.noreply.github.com>
2020-06-03 14:26:15 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 var headerID = d3.select("#headerID");
 console.log(headerID.text());
-headerID.text("Need to start to Java to understand D3");
+headerID.text("Need Java to understand D3");
 // d3.select();
 // d3.selectAll();
 // d3.select('hi').style('color','red')
