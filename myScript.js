7005b531b59aae2bd998752285fd6e7585809c1f
Thanh <thanhtrant219@gmail.com>
2020-06-03 13:54:50 -0500

    continue testing

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 var headerID = d3.select("#headerID");
 console.log(headerID.text());
-headerID.text("D3 had been completed");
+headerID.text("D3 had been not really maybe completed");
 // d3.select();
 // d3.selectAll();
 // d3.select('hi').style('color','red')
