f0ecefca3bdb6d41ee7852f3772455fa008e48c9
thanhtrant219 <63218294+thanhtrant219@users.noreply.github.com>
2020-06-04 12:18:12 -0500

    worldmap

--- myGraph1.js
+++ myGraph1.js
@@ -1,8 +1,27 @@
-var headerID = d3.select("#headerID");
-console.log(headerID.text());
-headerID.text("Java => understand D3 Graph1");
-// d3.select();
-// d3.selectAll();
-// d3.select('hi').style('color','red')
-// .attr('calss', 'heading');
-// .text('updated hello');
+(function (d3, topojson) {
+  'use strict';
+
+  const svg = d3.select('svg');
+
+  const projection = d3.geoOrthographic();
+  const pathGenerator = d3.geoPath().projection(projection);
+
+  Promise.all([
+    d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+    d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+  ]).then(([tsvData, topoJSONdata]) => {
+    const countryName = tsvData.reduce((accumulator,d) => {
+      accumulator[d.iso_n3] = d.name;
+      return accumulator;
+    },{});
+      
+      const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries);
+      svg.selectAll('path').data(countries.features)
+        .enter().append('path')
+          .attr('class', 'country')
+          .attr('d', pathGenerator)
+    			.append('title')
+    					.text(d => countryName[d.id]);
+    });
+
+}(d3, topojson));
