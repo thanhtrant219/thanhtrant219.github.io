5c328873113489c4d3e35dc58c5a1c4c177e8306
thanhtrant219 <63218294+thanhtrant219@users.noreply.github.com>
2020-06-04 18:43:45 -0500

    graph1

--- myGraph1.js
+++ myGraph1.js
@@ -1,98 +1,282 @@
-chart = {
-  const root = pack(data);
-  let focus = root;
-  let view;
 
-  const svg = d3.create("svg")
-      .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-      .style("display", "block")
-      .style("margin", "0 -14px")
-      .style("background", color(0))
-      .style("cursor", "pointer")
-      .on("click", () => zoom(root));
+// get the data
 
-  const node = svg.append("g")
-    .selectAll("circle")
-    .data(root.descendants().slice(1))
-    .join("circle")
-      .attr("fill", d => d.children ? color(d.depth) : "white")
-      .attr("pointer-events", d => !d.children ? "none" : null)
-      .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
-      .on("mouseout", function() { d3.select(this).attr("stroke", null); })
-      .on("click", d => focus !== d && (zoom(d), d3.event.stopPropagation()));
+links =  [
+  {
+    "source": "C",
+    "target": "Java",
+    "value": 1
+  },
+  {
+    "source": "JavaScript",
+    "target": "Java",
+    "value": 0
+  },
+  {
+    "source": "JavaScript",
+    "target": "Ruby",
+    "value": 1
+  },
+  {
+    "source": "Haskel",
+    "target": "Java",
+    "value": 0
+  },
+  {
+    "source": "Haskel",
+    "target": "JSON",
+    "value": 1
+  },
+  {
+    "source": "JavaScript",
+    "target": "Python",
+    "value": 0
+  },
+  {
+    "source": "Haskel",
+    "target": "Python",
+    "value": 1
+  },
+  {
+    "source": "Objact C",
+    "target": "Java",
+    "value": 0
+  },
+  {
+    "source": "Sharp",
+    "target": "Java",
+    "value": 1
+  },
+  {
+    "source": "Clojure",
+    "target": "C",
+    "value": 0
+  },
+  {
+    "source": "AWK",
+    "target": "C",
+    "value": 1
+  },
+  {
+    "source": "LISP",
+    "target": "JSON",
+    "value": 0
+  },
+  {
+    "source": "Cobra",
+    "target": "Python",
+    "value": 1
+  },
+  {
+    "source": "JSON",
+    "target": "Python",
+    "value": 0
+  },
+  {
+    "source": "Pascal",
+    "target": "Haskel",
+    "value": 1
+  },
+  {
+    "source": "PHP",
+    "target": "C",
+    "value": 0
+  },
+  {
+    "source": "Windows-Powershell",
+    "target": "Sharp",
+    "value": 1
+  },
+  {
+    "source": "Windows-Powershell",
+    "target": "Vala",
+    "value": 0
+  },
+  {
+    "source": "VBScript",
+    "target": "Objact C",
+    "value": 1
+  },
+  {
+    "source": "VisualBasic.NET",
+    "target": "Sharp",
+    "value": 0
+  },
+  {
+    "source": "AppleScript",
+    "target": "LISP",
+    "value": 1
+  },
+  {
+    "source": "C-Sharp",
+    "target": "Java",
+    "value": 0
+  },
+  {
+    "source": "Scala",
+    "target": "Java",
+    "value": 1
+  },
+  {
+    "source": "C++",
+    "target": "C",
+    "value": 0
+  },
+  {
+    "source": "Groovy",
+    "target": "Ruby",
+    "value": 1
+  },
+  {
+    "source": "Logo",
+    "target": "LISP",
+    "value": 0
+  },
+  {
+    "source": "Factor",
+    "target": "Haskel",
+    "value": 1
+  },
+  {
+    "source": "APL",
+    "target": "J",
+    "value": 0
+  },
+  {
+    "source": "J",
+    "target": "Java",
+    "value": 1
+  }
+];
 
-  const label = svg.append("g")
-      .style("font", "10px sans-serif")
-      .attr("pointer-events", "none")
-      .attr("text-anchor", "middle")
-    .selectAll("text")
-    .data(root.descendants())
-    .join("text")
-      .style("fill-opacity", d => d.parent === root ? 1 : 0)
-      .style("display", d => d.parent === root ? "inline" : "none")
-      .text(d => d.data.name);
+var nodes = {};
 
-  zoomTo([root.x, root.y, root.r * 2]);
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+    link.source = nodes[link.source] ||
+        (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] ||
+        (nodes[link.target] = {name: link.target});
+});
 
-  function zoomTo(v) {
-    const k = width / v[2];
+var width = 960,
+    height = 1000;
 
-    view = v;
+var force = d3.forceSimulation()
+    .nodes(d3.values(nodes))
+    .force("link", d3.forceLink(links).distance(100))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .force("charge", d3.forceManyBody().strength(-250))
+    .alphaTarget(1)
+    .on("tick", tick);
 
-    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("r", d => d.r * k);
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// add the links and the arrows
+
+var path = svg.append("g")
+  .selectAll("path")
+  .data(links)
+  .enter()
+  .append("path")
+  .attr("class", function(d) { return "link " + d.type; })
+  .attr('stroke-dasharray', function(d) {
+    if(d.value === 0) {
+      return '5, 5';
+    } else {
+      return null;
+    }
+  })
+  .style('stroke', function(d) {
+    if(d.value === 0) {
+      return 'black'
+    } else {
+      return 'green';
+    }
+  })
+  .style('stroke-width', function(d) {
+         if(d.value === 0) {
+    return 1;
+  } else {
+    return 3;
+  }
+  });
+
+// define the nodes
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+  .enter().append("g")
+    .attr("class", "node")
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended)
+      );
+
+// add the nodes
+node.append("circle")
+   .attr("r", function(d) {
+  d.weight = path.filter(function(links) {
+    return links.source.index == d.index || links.target.index == d.index
+  }).size();
+  return 3 + (d.weight * 2);
+  })
+    .style('fill', function(d, i) { 
+  console.log(d.weight/10)
+  return d3.interpolateBlues(d.weight/10); });
+  
+  
+//adding labels
+var label = node.append('text')
+  .text(function (d) {
+    return d.name; })
+  .attr('x', 12)   //moves label to the right of node
+  .attr('y', -12);  //moves label above node
+
+  
+// add the curvy lines
+function tick() {
+    path.attr("d", function(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" +
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
+    });
+
+    node
+        .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")"; })
+};
+
+function dragstarted(d) {
+      if (!d3.event.active) force.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    };
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+};
+
+function dragended(d) {
+  if (!d3.event.active) force.alphaTarget(0);
+  if (d.fixed == true){
+     d.fx = d.x;
+     d.fy = d.y;
+  }
+  else{
+    d.fx = null;
+    d.fy = null;
   }
 
-  function zoom(d) {
-    const focus0 = focus;
-
-    focus = d;
-
-    const transition = svg.transition()
-        .duration(d3.event.altKey ? 7500 : 750)
-        .tween("zoom", d => {
-          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-          return t => zoomTo(i(t));
-        });
-
-    label
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-      .transition(transition)
-        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-  }
-
-  return svg.node();
-}
-
-data = Object {
-  name: "flare"
-  children: Array(10) [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object]
-}
-
-data = FileAttachment("flare-2.json").json()
-
-pack = ƒ(data)
-
-pack = data => d3.pack()
-    .size([width, height])
-    .padding(3)
-  (d3.hierarchy(data)
-    .sum(d => d.value)
-    .sort((a, b) => b.value - a.value))
-
-  width = 932
-  width = 932
-  height = 932
-  height = width
-  format = ƒ(t)
-  format = d3.format(",d")
-  color = ƒ(n)
-  color = d3.scaleLinear()
-    .domain([0, 5])
-    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-    .interpolate(d3.interpolateHcl)
-  d3 = Object {event: null, format: ƒ(t), formatPrefix: ƒ(t, n), timeFormat: ƒ(t), timeParse: ƒ(t), utcFormat: ƒ(t), utcParse: ƒ(t), FormatSpecifier: ƒ(t), active: ƒ(t, n), arc: ƒ(), area: ƒ(), areaRadial: ƒ(), ascending: ƒ(t, n), autoType: ƒ(t), axisBottom: ƒ(t), axisLeft: ƒ(t), axisRight: ƒ(t), axisTop: ƒ(t), bisect: ƒ(n, e, r, i), bisectLeft: ƒ(n, e, r, i), …}
-  d3 = require("d3@5")
+};
