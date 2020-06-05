d7ede30733701db1312d5176b4d45893c92e4d2f
thanhtrant219 <63218294+thanhtrant219@users.noreply.github.com>
2020-06-04 19:32:06 -0500

    d

--- myGraph2.js
+++
@@ -1,8 +0,0 @@
-var headerID = d3.select("#headerID");
-console.log(headerID.text());
-headerID.text("Java => understand D3 Graph2 ");
-// d3.select();
-// d3.selectAll();
-// d3.select('hi').style('color','red')
-// .attr('calss', 'heading');
-// .text('updated hello');
